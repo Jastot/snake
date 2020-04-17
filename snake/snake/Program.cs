ee551019be60fd3cb143c2df48a25d97f807d79b
Jastot xD <george1005@yandex.ru>
2020-04-17 19:30:58 +0300

    Lesson 6

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 
 namespace snake
@@ -7,14 +8,35 @@ namespace snake
     {
         static void Main(string[] args)
         {
+           
             Point p1 = new Point(1,3,'*');
             p1.Draw();
-
+            
             Point p2 = new Point(4,5,'#');
             p2.Draw();
 
-           Console.ReadLine();
-        }
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
 
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach(int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+
+
+
+            Console.ReadLine();
+        }
+    
     }
 }
